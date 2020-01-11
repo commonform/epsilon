0a3a5c5704feecd4ebf2da8cd1a22259a4e947e8
Kyle E. Mitchell <kyle@kemitchell.com>
2020-01-10 20:52:53 -0800

    Log with Pino

---
+++ server.js
@@ -0,0 +1,33 @@
+var handler = require('./')
+var http = require('http')
+var pino = require('pino')
+var pinoHTTP = require('pino-http')
+var uuid = require('uuid')
+
+var log = pino({ server: uuid.v4() })
+
+var server = http.createServer(function (request, response) {
+  pinoHTTP({ logger: log, genReqId: uuid.v4 })(request, response)
+  handler(request, response)
+})
+
+function close () {
+  log.info('closing')
+  server.close(function () {
+    log.info('closed')
+    process.exit(0)
+  })
+}
+
+process.on('SIGINT', close)
+process.on('SIGQUIT', close)
+process.on('SIGTERM', close)
+process.on('uncaughtException', function (exception) {
+  log.error(exception)
+  close()
+})
+
+server.listen(process.env.PORT || 8080, function () {
+  // If the environment set PORT=0, we'll get a random high port.
+  log.info({ port: this.address().port }, 'listening')
+})
--- test/server.js
+++ test/server.js
@@ -1,14 +1,21 @@
 var assert = require('assert')
 var fs = require('fs')
-var http = require('http')
 var handler = require('../')
+var http = require('http')
+var pino = require('pino')
+var pinoHTTP = require('pino-http')
 var rimraf = require('rimraf')
+var uuid = require('uuid')
 
 module.exports = function testServer (callback) {
   assert(typeof callback === 'function')
+  var log = pino({}, fs.createWriteStream('test-server.log'))
   fs.mkdtemp('/tmp/', function withDirectory (ignore, directory) {
     process.env.DIRECTORY = directory
-    var server = http.createServer(handler)
+    var server = http.createServer((request, response) => {
+      pinoHTTP({ logger: log, genReqId: uuid.v4 })(request, response)
+      handler(request, response)
+    })
     server.listen(0, function () {
       callback(this.address().port, function () {
         server.close(function () {
