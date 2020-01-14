097edf434a08fa8050e7edc2c8c19c4a088e6e68
Kyle E. Mitchell <kyle@kemitchell.com>
2020-01-13 17:36:52 -0800

    Authenticate /form requests

--- routes/forms.js
+++ routes/forms.js
@@ -1,4 +1,5 @@
 var DIGEST_RE = require('../util/digest-re')
+var authenticate = require('./authenticate')
 var form = require('./partials/form')
 var head = require('./partials/head')
 var header = require('./partials/header')
@@ -12,27 +13,28 @@ var storage = require('../storage')
 
 module.exports = (request, response) => {
   if (request.method !== 'GET') return methodNotAllowed(request, response)
-  var digest = request.parameters.digest
-  var json
-  if (digest.endsWith('.json')) {
-    json = true
-    digest = digest.replace(/\.json$/, '')
-  }
-  if (!DIGEST_RE.test(digest)) return notFound(request, response)
-  if (json) {
-    response.setHeader('Content-Type', 'application/json')
-    return pump(
-      storage.form.createRawReadStream(digest),
-      response
-    )
-  }
-  storage.form.read(digest, (error, rawForm) => {
-    if (error) return internalError(request, response, error)
-    if (!rawForm) return notFound(request, response)
-    loadComponents(rawForm, {}, (error, loadedForm, resolutions) => {
+  authenticate(request, response, () => {
+    var digest = request.parameters.digest
+    var json
+    if (digest.endsWith('.json')) {
+      json = true
+      digest = digest.replace(/\.json$/, '')
+    }
+    if (!DIGEST_RE.test(digest)) return notFound(request, response)
+    if (json) {
+      response.setHeader('Content-Type', 'application/json')
+      return pump(
+        storage.form.createRawReadStream(digest),
+        response
+      )
+    }
+    storage.form.read(digest, (error, rawForm) => {
       if (error) return internalError(request, response, error)
-      response.setHeader('Content-Type', 'text/html')
-      response.end(`
+      if (!rawForm) return notFound(request, response)
+      loadComponents(rawForm, {}, (error, loadedForm, resolutions) => {
+        if (error) return internalError(request, response, error)
+        response.setHeader('Content-Type', 'text/html')
+        response.end(`
 <!doctype html>
 <html lang=en-US>
   ${head()}
@@ -45,6 +47,7 @@ module.exports = (request, response) => {
   </body>
 </html>
       `.trim())
+      })
     })
   })
 }
