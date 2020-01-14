ebac66abdfe0054f3de896f53ab46c1b6ef24583
Kyle E. Mitchell <kyle@kemitchell.com>
2020-01-13 23:14:35 -0800

    Spawn ChromeDriver from Node.js

--- test/webdriver.js
+++ test/webdriver.js
@@ -1,15 +1,31 @@
+var path = require('path')
+var spawn = require('child_process').spawn
+var tape = require('tape')
 var webdriverio = require('webdriverio')
 
 // See: https://webdriver.io/docs/runprogrammatically.html
 
-var remote = webdriverio.remote({
-  logLevel: 'error',
-  host: 'localhost',
-  port: 9515,
-  path: '/',
-  capabilities: { browserName: 'chrome' }
-})
+var chromedriver = spawn(
+  path.join(__dirname, '..', 'node_modules', '.bin', 'chromedriver')
+)
+
+var remote
 
 module.exports = function () {
+  if (!remote) {
+    remote = webdriverio.remote({
+      logLevel: 'error',
+      host: 'localhost',
+      port: 9515,
+      path: '/',
+      capabilities: { browserName: 'chrome' }
+    })
+  }
   return remote
 }
+
+tape.onFinish(() => {
+  remote
+    .then((browser) => browser.deleteSession())
+    .then(() => chromedriver.kill())
+})
