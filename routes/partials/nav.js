63d6660385743f0c47360b1d9d52e5108bf85b40
Kyle E. Mitchell <kyle@kemitchell.com>
2020-01-13 18:25:19 -0800

    Rename "Edit" to "New form" in <nav>

--- routes/partials/nav.js
+++ routes/partials/nav.js
@@ -4,7 +4,7 @@ module.exports = (session) => {
     <nav role=navigation>
       ${!handle ? '<a class=button href=/login>Log In</a>' : ''}
       ${!handle ? '<a class=button href=/signup>Sign Up</a>' : ''}
-      ${handle ? '<a class=button href=/edit>Edit</a>' : ''}
+      ${handle ? '<a class=button href=/edit>New Form</a>' : ''}
       ${handle ? '<a class=button href=/logout>Log Out</a>' : ''}
       ${handle ? '<a class=button href=/account>Account</a>' : ''}
     </nav>
