7e8890263c85ad57b0bc011c6f64575ab4683533
Kyle E. Mitchell <kyle@kemitchell.com>
2020-01-13 18:20:35 -0800

    Add logo to <header>

--- routes/partials/header.js
+++ routes/partials/header.js
@@ -1,6 +1,8 @@
 module.exports = () => {
+  var logo = '<img id=logo src=/logo.svg>'
   return `
 <header role=banner>
+  ${process.env.NODE_ENV === 'test' ? '' : logo}
   <h1>Common Form</h1>
 </header>
   `.trim()
