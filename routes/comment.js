bb75056f14239ba55ef0a1852246d248ddf29f89
Kyle E. Mitchell <kyle@kemitchell.com>
2020-01-20 19:14:15 -0800

    Validate comment UUIDs

--- routes/comment.js
+++ routes/comment.js
@@ -1,3 +1,4 @@
+const UUID_RE = require('../util/uuid-re')
 const found = require('./found')
 const internalError = require('./internal-error')
 const methodNotAllowed = require('./method-not-allowed')
@@ -11,9 +12,10 @@ module.exports = (request, response) => {
 
 function get (request, response) {
   const id = request.parameters.id
-  if (!id) return notFound(request, response)
+  if (!id || !UUID_RE.test(id)) return notFound(request, response)
   storage.comment.read(id, (error, comment) => {
     if (error) return internalError(request, response, error)
+    if (!comment) return notFound(request, response)
     const location = `/forms/${comment.context}#${id}`
     found(request, response, location)
   })
