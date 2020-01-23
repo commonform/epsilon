e2b2408ad05f24f53bb2f273f6d25777988b5fad
Kyle E. Mitchell <kyle@kemitchell.com>
2020-01-22 23:42:28 -0800

    Refactor /handle with form route helper function

--- routes/handle.js
+++ routes/handle.js
@@ -1,27 +1,22 @@
-const Busboy = require('busboy')
-const escape = require('../util/escape')
+const EMAIL_RE = require('../util/email-re')
+const formRoute = require('./form-route')
 const handleNotification = require('../notifications/handle')
 const head = require('./partials/head')
 const header = require('./partials/header')
 const html = require('./html')
 const indexes = require('../indexes')
-const runSeries = require('run-series')
 
-module.exports = function (request, response) {
-  const method = request.method
-  if (method === 'GET') return get(request, response)
-  if (method === 'POST') return post(request, response)
-  response.statusCode = 405
-  response.end()
+const fields = {
+  email: {
+    filter: (e) => e.toLowerCase().trim(),
+    validate: (e) => EMAIL_RE.test(e)
+  }
 }
 
-function get (request, response, error) {
-  const message = request.query.message || error
-  const messageParagraph = message
-    ? `<p class=message>${escape(message)}</p>`
-    : ''
-  response.setHeader('Content-Type', 'text/html')
-  response.end(html`
+module.exports = formRoute({ form, fields, processBody, onSuccess })
+
+function form (request, data) {
+  return html`
 <!doctype html>
 <html lang=en-US>
   ${head()}
@@ -29,37 +24,29 @@ function get (request, response, error) {
     ${header()}
     <main role=main>
       <h2>Forgot Handle</h2>
-      ${messageParagraph}
       <form method=post>
+        ${data.error}
         <p>
           <label for=email>E-Mail</label>
-          <input name=email type=email required autofocus autocomplete=off>
+          <input
+              name=email
+              type=email
+              required
+              autofocus
+              autocomplete=off>
         </p>
+        ${data.email.error}
         <button type=submit>Send Handle</button>
       </form>
     </main>
   </body>
 </html>
-  `)
+  `
 }
 
-function post (request, response) {
-  let email
-  runSeries([
-    readPostBody,
-    sendEMail
-  ], function (error) {
-    if (error) {
-      if (error.statusCode === 401) {
-        response.statusCode = 401
-        return get(request, response, error.message)
-      }
-      request.log.error(error)
-      response.statusCode = error.statusCode || 500
-      return response.end()
-    }
-    response.setHeader('Content-Type', 'text/html')
-    response.end(html`
+function onSuccess (request, response, body) {
+  response.setHeader('Content-Type', 'text/html')
+  response.end(html`
 <!doctype html>
 <html lang=en-US>
   ${head()}
@@ -71,35 +58,17 @@ function post (request, response) {
     </main>
   </body>
 </html>
-    `)
+  `)
+}
+
+function processBody (request, body, done) {
+  const email = body.email
+  indexes.email.read(email, (error, handle) => {
+    if (error) return done(error)
+    if (!handle) return done()
+    handleNotification({
+      to: email,
+      handle
+    }, done)
   })
-
-  function readPostBody (done) {
-    request.pipe(
-      new Busboy({
-        headers: request.headers,
-        limits: {
-          fieldNameSize: 6,
-          fieldSize: 64,
-          fields: 1,
-          parts: 1
-        }
-      })
-        .on('field', function (name, value, truncated, encoding, mime) {
-          if (name === 'email') email = value.toLowerCase()
-        })
-        .once('finish', done)
-    )
-  }
-
-  function sendEMail (done) {
-    indexes.email.read(email, (error, handle) => {
-      if (error) return done(error)
-      if (!handle) return done()
-      handleNotification({
-        to: email,
-        handle
-      }, done)
-    })
-  }
 }
